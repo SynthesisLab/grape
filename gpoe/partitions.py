f58683fdee0d50b1af0c3fe31834a8ea23f6eb09
Baptiste <baptiste.mouillon21@gmail.com>
2025-03-27 14:59:27 +0100

    Optimized generation

--- gpoe/partitions.py
+++ gpoe/partitions.py
@@ -1,15 +1,15 @@
-from itertools import combinations_with_replacement, permutations
 from typing import Generator, Tuple
 
 
 def integer_partitions(k: int, n: int) -> Generator[Tuple[int, ...], None, None]:
-    choices = list(range(1, n - k + 2))
-    permut = permutations(range(k))
-    for elements in combinations_with_replacement(choices, k):
-        if sum(elements) == n:
-            done = set()
-            for p in permut:
-                new = tuple(elements[i] for i in p)
-                if new not in done:
-                    yield new
-                    done.add(new)
+    tup = [n - k + 1] + [1] * (k - 1)
+    yield tuple(tup)
+    while True:
+        if tup[-1] == n - k + 1: break
+        carry = tup[-1] - 1
+        tup[-1] = 1
+        for i in range(k - 2, -1, -1):
+            if tup[i] > 1: break
+        tup[i] -= 1
+        tup[i + 1] += 1 + carry
+        yield tuple(tup)
