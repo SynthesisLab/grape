4ecaecaee154cdccd8cf4a2e8929a430d31465c3
Théo Matricon <theomatricon@gmail.com>
2025-05-16 17:33:10 +0200

    added tests for prune and loop manager

---
+++ tests/automaton/test_loop_manager.py
@@ -0,0 +1,115 @@
+import random
+from grape.automaton.loop_manager import LoopStrategy, add_loops
+from grape.automaton.spec_manager import specialize
+from grape.automaton.tree_automaton import DFTA
+from grape.automaton_generator import grammar_by_saturation
+from grape.dsl import DSL
+from grape.enumerator import Enumerator
+from grape.evaluator import Evaluator
+from grape.pruning.equivalence_class_manager import EquivalenceClassManager
+from grape.pruning.obs_equiv_pruner import prune
+
+
+def sample_inputs(nsamples: int, sample_dict: dict[str, callable]) -> dict[str, list]:
+    inputs = {}
+    for sampled_type, sample_fn in sample_dict.items():
+
+        def eq_fn(x, y):
+            return x == y
+
+        sampled_inputs = []
+        tries = 0
+        while len(sampled_inputs) < nsamples and tries < 100:
+            sampled = sample_fn()
+            if all(not eq_fn(sampled, el) for el in sampled_inputs):
+                sampled_inputs.append(sampled)
+                tries = 0
+            tries += 1
+        while len(sampled_inputs) < nsamples:
+            sampled_inputs += sampled_inputs
+            sampled_inputs = sampled_inputs[:nsamples]
+
+        inputs[sampled_type] = sampled_inputs
+    return inputs
+
+
+random.seed(1)
+INT_SIZE = 32
+MAXI = (1 << INT_SIZE) - 1
+assert len(f"{MAXI:b}") == INT_SIZE, len(f"{MAXI:b}")
+# Given a type provides a function to sample one element
+
+
+def signed_masking(n: int, mask: int = MAXI) -> int:
+    return n & mask if n > 0 else -(-n & mask)
+
+
+sample_dict = {"int": lambda: signed_masking(random.randint(-MAXI, MAXI), MAXI)}
+
+inputs = sample_inputs(50, sample_dict)
+
+
+dsl = DSL(
+    {
+        "1": ("int", 1),
+        "0": ("int", 0),
+        "+": ("int -> int -> int", lambda x, y: x + y),
+        "*": ("int -> int -> int", lambda x, y: x * y),
+        "-": ("int -> int", lambda x: -x),
+        "True": ("bool", True),
+        ">0": ("int -> bool", lambda x: x > 0),
+        "ite": (
+            "bool -> 'a [bool|int] -> 'a -> 'a",
+            lambda b, pos, neg: pos if b else neg,
+        ),
+    }
+)
+evaluator = Evaluator(dsl, inputs, {}, set())
+max_size = 4
+manager = EquivalenceClassManager()
+out, tr = prune(dsl, evaluator, manager, max_size=max_size, rtype="int")
+tr = "int->int"
+saturated = grammar_by_saturation(dsl, tr)
+
+
+def comp_by_enum(grammars: list, tr: str, max_size: int):
+    enums: list[Enumerator] = []
+    for g in grammars:
+        e = Enumerator(g)
+        gen = e.enumerate_until_size(max_size + 1)
+        p = next(gen)
+        evaluator.eval(p, tr)
+        should_keep = True
+        try:
+            while True:
+                p = gen.send(should_keep)
+                should_keep = evaluator.eval(p, tr) is None
+        except StopIteration:
+            pass
+        enums.append(e)
+    std = enums.pop()
+    for other in enums:
+        for size in range(max_size + 1):
+            a = other.count_programs_at_size(size)
+            b = std.count_programs_at_size(size)
+            if a != b:
+                programs = []
+                for state in other.states:
+                    programs += other.memory[state][size]
+                p2 = []
+                for state in std.states:
+                    p2 += std.memory[state][size]
+                assert set(p2) == set(programs)
+
+
+def test_same_size():
+    out_proc = dsl.merge_type_variants(add_loops(out, dsl, LoopStrategy.STATE))
+    comp_by_enum([out, out_proc], tr, max_size)
+    spec_out = specialize(out_proc, tr, dsl)
+    comp_by_enum([spec_out, saturated], tr, max_size)
+
+
+def test_next_size():
+    out_proc = dsl.merge_type_variants(add_loops(out, dsl, LoopStrategy.STATE))
+    spec_out = specialize(out_proc, tr, dsl)
+    comp_by_enum([saturated, spec_out], tr, max_size + 1)
---
+++ tests/cli/test_prune.py
@@ -0,0 +1,137 @@
+import random
+from grape.automaton.loop_manager import LoopStrategy, add_loops
+from grape.automaton.spec_manager import specialize
+from grape.automaton.tree_automaton import DFTA
+from grape.automaton_generator import grammar_by_saturation
+from grape.dsl import DSL
+from grape.enumerator import Enumerator
+from grape.evaluator import Evaluator
+from grape.pruning.equivalence_class_manager import EquivalenceClassManager
+from grape.pruning.obs_equiv_pruner import prune
+
+
+def sample_inputs(nsamples: int, sample_dict: dict[str, callable]) -> dict[str, list]:
+    inputs = {}
+    for sampled_type, sample_fn in sample_dict.items():
+
+        def eq_fn(x, y):
+            return x == y
+
+        sampled_inputs = []
+        tries = 0
+        while len(sampled_inputs) < nsamples and tries < 100:
+            sampled = sample_fn()
+            if all(not eq_fn(sampled, el) for el in sampled_inputs):
+                sampled_inputs.append(sampled)
+                tries = 0
+            tries += 1
+        while len(sampled_inputs) < nsamples:
+            sampled_inputs += sampled_inputs
+            sampled_inputs = sampled_inputs[:nsamples]
+
+        inputs[sampled_type] = sampled_inputs
+    return inputs
+
+
+random.seed(1)
+INT_SIZE = 32
+MAXI = (1 << INT_SIZE) - 1
+assert len(f"{MAXI:b}") == INT_SIZE, len(f"{MAXI:b}")
+# Given a type provides a function to sample one element
+
+
+def signed_masking(n: int, mask: int = MAXI) -> int:
+    return n & mask if n > 0 else -(-n & mask)
+
+
+sample_dict = {"int": lambda: signed_masking(random.randint(-MAXI, MAXI), MAXI)}
+
+inputs = sample_inputs(50, sample_dict)
+
+
+dsl = DSL(
+    {
+        "1": ("int", 1),
+        "0": ("int", 0),
+        "+": ("int -> int -> int", lambda x, y: x + y),
+        "*": ("int -> int -> int", lambda x, y: x * y),
+        "-": ("int -> int", lambda x: -x),
+        "True": ("bool", True),
+        ">0": ("int -> bool", lambda x: x > 0),
+        "ite": (
+            "bool -> 'a [bool|int] -> 'a -> 'a",
+            lambda b, pos, neg: pos if b else neg,
+        ),
+    }
+)
+evaluator = Evaluator(dsl, inputs, {}, set())
+max_size = 4
+
+
+def comp_by_enum(grammars: list, tr: str, max_size: int):
+    enums: list[Enumerator] = []
+    for g in grammars:
+        e = Enumerator(g)
+        gen = e.enumerate_until_size(max_size)
+        p = next(gen)
+        evaluator.eval(p, tr)
+        should_keep = True
+        try:
+            while True:
+                p = gen.send(should_keep)
+                should_keep = evaluator.eval(p, tr) is None
+        except StopIteration:
+            pass
+        enums.append(e)
+    std = enums.pop()
+    for other in enums:
+        for size in range(max_size):
+            assert other.count_programs_at_size(size) == std.count_programs_at_size(
+                size
+            )
+
+
+def test_prune():
+    manager = EquivalenceClassManager()
+    out, tr = prune(dsl, evaluator, manager, max_size=max_size, rtype="int")
+    tr = "int->int"
+    g = grammar_by_saturation(dsl, tr)
+    spec_out = specialize(out, tr, dsl)
+    comp_by_enum([spec_out, g], tr, max_size + 1)
+
+
+def postprocess(grammar: DFTA, type_req: str, no_loop: bool = False) -> DFTA:
+    grammar = add_loops(
+        grammar,
+        dsl,
+        LoopStrategy.NO_LOOP if no_loop else LoopStrategy.STATE,
+    )
+    grammar = dsl.merge_type_variants(grammar)
+    return grammar
+
+
+def test_incremental_same_size():
+    manager = EquivalenceClassManager()
+    out, tr = prune(dsl, evaluator, manager, max_size=max_size, rtype="int")
+    out_looped = postprocess(out, tr)
+    incremental, tr = prune(
+        dsl, evaluator, manager, max_size=max_size, rtype="int", base_grammar=out
+    )
+    incremental_looped = postprocess(incremental, tr)
+    assert incremental.trees_until_size(50) == out.trees_until_size(50)
+    assert incremental_looped.trees_until_size(50) == out_looped.trees_until_size(50)
+
+
+def test_incremental_next_size():
+    manager = EquivalenceClassManager()
+    out, tr = prune(dsl, evaluator, manager, max_size=max_size, rtype="int")
+    out = postprocess(out, tr)
+    print(out)
+    incremental, tr = prune(
+        dsl, evaluator, manager, max_size=max_size + 1, rtype="int", base_grammar=out
+    )
+    incremental = postprocess(incremental, tr)
+    direct, tr = prune(dsl, evaluator, manager, max_size=max_size + 1, rtype="int")
+    direct = postprocess(direct, tr)
+
+    assert incremental.trees_until_size(50) == direct.trees_until_size(50)
